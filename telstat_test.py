12a8d107f16f43e7b63d3525198575759df87364
Edward Gomez <edward@gomez.me.uk>
2017-12-04 10:28:08 +0000

    New feature refactoring

--- telstat_test.py
+++ telstat_test.py
@@ -19,21 +19,7 @@ def main():
         soup = BeautifulSoup(posfeed)
         alt = soup.findAll('td',attrs={"name":"alt_demand_value"})
         az = soup.findAll('td',attrs={"name":"az_actual_value"})
-        if alt:
-            axis_alt = int(float(alt[teli].string))
-            if axis_alt < 20:
-                axis_alt = 90
-            else
-                axis_alt = axis_alt+90
-            axis_alt = chr(axis_alt)
-        if az:
-            axis_az = int(float(az[teli].string))
-            if (axis_az > 170):
-                axis_az = axis_az - 180
-            elif (axis_az < 20):
-                axis_az = 90
-            axis_az = chr(axis_az)
-        
+
 
 if __name__ == '__main__':
     main()
